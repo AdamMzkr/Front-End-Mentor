5b856c57a1966022318d20a82826400020fdd69e
AdamMzkr <93909097+AdamMzkr@users.noreply.github.com>
2022-07-06 20:03:01 +0200

    check all inputs are ampty and validate email

--- intro-form/main.js
+++ intro-form/main.js
@@ -1,26 +1,67 @@
+//inpputs
 const name = document.querySelector('#firstN');
+const lName= document.querySelector('#lastN');
+const email= document.querySelector('#email');
+const password =document.querySelector('#password');
+//errors
+const errors =document.querySelectorAll('.errorMes');
+
+//submit button
 const submit= document.querySelector('#submit');
-const error =document.querySelector('#errorM');
 
-submit.addEventListener('click', validate);
 
-function validate(e) {
-  e.preventDefault();
+submit.addEventListener( 'click', function(e){
+
+
+
+e.preventDefault();
+
+validateNotEmpty(name,errors[0]);
+validateNotEmpty(lName,errors[1]);
+validateEmail(email,errors[2]);
+validateNotEmpty(password,errors[3]);
+
+}
+  );
+
+
+function validateNotEmpty(input, error) {
 
   let valid = true;
 
-  if (!name.value) {
+  if (!input.value) {
 
     error.classList.add("visible");
-    name.classList.add("error");
-    name.placeholder='';
+    input.classList.add("error");
+    input.placeholder='';
     error.setAttribute('aria-hidden', false);
     error.setAttribute('aria-invalid', true);
   }
 
   else{
-  	error.classList.remove("visible");
-    name.classList.remove("error");
+  error.classList.remove("visible");
+  input.classList.remove("error");
+  	
   }
   return valid;
 }
+
+
+function validateEmail(mail,error) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    error.classList.remove("visible");
+    input.classList.remove("error");
+
+    return (true)
+  }
+    error.classList.add("visible");
+    mail.classList.add("error");
+    mail.placeholder='example@mail.com';
+    error.setAttribute('aria-hidden', false);
+    error.setAttribute('aria-invalid', true);
+    
+    return (false)
+}
+
